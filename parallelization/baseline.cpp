662ec03caeebc82442e5e90d5a10cc9eef0fd86e
Allison Chilton <achilton@salem.cs.colostate.edu>
2020-12-02 23:52:36 -0700

    read file

--- parallelization/baseline.cpp
+++ parallelization/baseline.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include "rs.hpp"
 #include "string.h"
+#include "timer.h"
+
 using namespace std;
 
 #define ECC_LENGTH 16
@@ -24,7 +26,6 @@ msg_buffers split_messages(char* message, int message_length){
         printf("Buffers must be divisible by chunksize %d. Message is length: %d. Aborting\n", chunksize, message_length);
         exit(1);
     }
-        ++chunks;
     
     // make a list of buffer pointers to contain the split messages
     char** buffers = (char**) malloc(sizeof(char*) * chunks);
@@ -41,25 +42,66 @@ msg_buffers split_messages(char* message, int message_length){
     return retval;
 }
 
+#ifndef VERBOSE
+#define VERBOSE 0
+#endif
+#define PRINT_TIMING
+
+double get_time(){
+  stop_timer();
+  double retval = elapsed_time();
+  reset_timer();
+  start_timer();
+  return retval;
+}
+
 int main() {
 
-    char messagelong[] = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America. All legislative Powers herein granted shall be vested in a Congress of the United States, which shall consist of a Senate and House of Representative";
+    FILE* fp = fopen("examplefile.txt","r");
+    fseek(fp, 0, SEEK_END);
+    int fsize = ftell(fp);
+    int chunks = fsize / (255-ECC_LENGTH);
+    int msglength = chunks * (255-ECC_LENGTH); // truncate to nearest block size for simplicity
+    fseek(fp, 0, SEEK_SET);
+    char *messagelong = (char*) malloc(msglength);
+    if(messagelong == 0){
+        printf("Malloc failed. Aborting\n");
+        exit(1);
+    }
+    fread(messagelong, 1, msglength, fp);
+    fclose(fp);
 
-    int msglength = sizeof(messagelong);
+    /* Timer setup */
+    initialize_timer();
+    start_timer();
+    double timings[10];
+    int timeIdx = 0;
 
-    msg_buffers buffers = split_messages(messagelong, msglength);
 
+    /* initialization */
     const int chunklen = 255 - ECC_LENGTH;
-
-    char repaired[msglength];
-    char encoded[msglength + (ECC_LENGTH * buffers.length)];
-
-
+    chunks = msglength / chunklen;
+    char* repaired = (char*) malloc(sizeof(char) * msglength);
+    char* encoded = (char*) malloc(sizeof(char) * (msglength + (ECC_LENGTH * chunks)));
     RS::ReedSolomon<chunklen, ECC_LENGTH> rs;
 
+    timings[timeIdx++] = get_time();
+
+    /* split buffers */
+    msg_buffers buffers = split_messages(messagelong, msglength);
+    timings[timeIdx++] = get_time();
+
+    /* encode */
     for(int i = 0; i < buffers.length; ++i){
         char* encode_dest = encoded + ((buffers.chunksize + ECC_LENGTH) * i);
         rs.Encode(buffers.buffers[i], encode_dest);
+    }
+
+    timings[timeIdx++] = get_time();
+
+    /* corrupt */
+    for(int i = 0; i < buffers.length; ++i){
+        char* encode_dest = encoded + ((buffers.chunksize + ECC_LENGTH) * i);
 
         // Corrupting maximum amount per chunk
         for(uint i = 0; i < 0+(ECC_LENGTH / 2); i++) {
@@ -67,16 +109,30 @@ int main() {
         }
     }
 
+    timings[timeIdx++] = get_time();
+
+    /* decode */
     for(int i = 0; i < buffers.length; ++i){
         char* encode_src = encoded + ((buffers.chunksize + ECC_LENGTH) * i);
         rs.Decode(encode_src, repaired + (i * buffers.chunksize));
     }
 
-    std::cout << "Original:  " << messagelong  << std::endl;
-    std::cout << "Corrupted: " << encoded  << std::endl;
-    std::cout << "Repaired:  " << repaired << std::endl;
+    timings[timeIdx++] = get_time();
+
+    if(VERBOSE){
+        std::cout << "Original:  " << messagelong  << std::endl;
+        std::cout << "Corrupted: " << encoded  << std::endl;
+        std::cout << "Repaired:  " << repaired << std::endl;
+    }
 
     std::cout << ((memcmp(messagelong, repaired, msglength) == 0) ? "SUCCESS" : "FAILURE") << std::endl;
+    #ifdef PRINT_TIMING
+    for(int i = 0; i < timeIdx; ++i){
+        printf("Checkpoint %d: %.6f\n",  i, timings[i]);
+    }
+    #endif
+
+    free(encoded); free(messagelong); free(repaired);
     return 0;
 }
 
